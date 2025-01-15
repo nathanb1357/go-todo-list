5f5e43c874591c556c7670594c2b8d8a9dfe1f7c
Nathan Bartyuk <nathanb1357@gmail.com>
2025-01-14 16:49:44 -0800

    Added task structs and cobra setip

---
+++ main.go
@@ -0,0 +1,9 @@
+/* Copyright © 2025 NATHAN BARTYUK */
+
+package main
+
+import "github.com/nathanb1357/go-todo-list/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ tasks.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+// A task with a status, name, and ID
+type Task struct {
+	ID        int
+	Name      string
+	Completed bool
+}
+
+// A manager struct that stores a list of tasks
+type TaskManager struct {
+	Tasks []Task
+}
+
+// Method that adds a new task to the list based on the description
+func (manager *TaskManager) AddTask(name string) {
+	newTask := Task{
+		ID:        len(manager.Tasks) + 1,
+		Name:      name,
+		Completed: false,
+	}
+	manager.Tasks = append(manager.Tasks, newTask)
+}
+
+// Marks a task as complete
+func (manager *TaskManager) CompleteTask(id int) error {
+	for i, task := range manager.Tasks {
+		if task.ID == id {
+			manager.Tasks[i].Completed = true
+			return nil
+		}
+	}
+	return errors.New("task not found")
+}
+
+// Removes a task from the list
+func (manager *TaskManager) RemoveTask(id int) error {
+	for i, task := range manager.Tasks {
+		if task.ID == id {
+			manager.Tasks = append(manager.Tasks[:i], manager.Tasks[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("task not found")
+}
+
+// Saves the current task list into a specified file
+func (manager *TaskManager) SaveToFile(fileName string) error {
+	encoded, err := json.Marshal(manager.Tasks)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile("temp.json", encoded, 0644)
+}
+
+// Loads the current task list from a specified file
+func (manager *TaskManager) LoadFromFile(fileName string) error {
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(file, &manager.Tasks)
+}
